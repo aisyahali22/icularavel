410af0535a40cb2dbb55f57d0fddf09a67e833c7
aisyahali <aisyah.ali@icu.gov.my>
2024-10-15 10:37:14 +0800

    1. home tambah hardcode 2. web tambah route baru

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,49 @@ route::get('/home', function(){
     return view('home');
 });
 
+route::get('/home2/{name}', function($name){
+    return view('home',['name'=>$name]);
+});
+
 route::get('/auth/signIn', function(){
     return view('auth.signin');
+});
+
+route::get('/user/profile', function(){
+    return 'Peengguna baru perlu mendaftar';
+})->name('user.profile');
+
+//route param
+route::get('/user/{name}', function($name){
+return 'User berdaftar '.$name;
+});
+
+route::get('/user/{name}/{age}', function($name, $age){
+    return 'User '.$name. 'age '.$age;
+});
+
+
+
+//redirect route to named route
+route::get('/redirect-to-profile', function(){
+return redirect()->route('user.profile');
+});
+
+route::prefix('food')->group(function(){
+    route::get('/details', function(){
+        return 'Food details are following';
+    });
+    route::get('/home2', function(){
+        return 'Food home page';
+    });
+});
+
+route::name('job')->prefix('job')->group(function(){
+    route::get('/home', function(){
+        return 'Job home page';
+    })->name('.home');
+
+    route::get('/description', function(){
+        return 'Job details are following';
+    })->name('.description');
 });
